906c45869e07dcb243a35ba945198ca723269124
jacklusy <jackalloussi23@gmail.com>
2023-01-24 16:28:01 +0300

    pages

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,30 @@
 @include('navbar')
 
+    <link rel="stylesheet" href="css/about.css">
+    <div class="About">
+        <div class="Details">
+            <h1>
+                About US
+            </h1>
+            <p>
+                It is a long established fact that a reader will be distracted by the readable content of a page when
+                looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution
+                of letters, as opposed to using 'Content here, content here', making it look like readable English. Many
+                desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a
+                search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have
+                evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                <div class="icons">
+                    <i style="font-size:24px" class="fa">&#xf230;</i>
+                    <i style="font-size:24px" class="fa">&#xf0d5;</i>
+                    <i style="font-size:24px" class="fa">&#xf0e1;</i>
+                </div>
+                <h3>example@gmail.com</h3>
+            </p>
+        </div>
+        <div class="image">
+            <img src="assets/slide-image-free-img.jpeg">
+        </div>
+    </div>
+
+
 @include('footer')
--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,4 +1,5 @@
 
+<link rel="stylesheet" href="css/style.css">
     <!-- Footer Starts-->
     <footer>
         <div class="container">
