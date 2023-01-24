cb736698af7d9ed58eca6d24ac6f32700845a0f5
jacklusy <jackalloussi23@gmail.com>
2023-01-24 09:45:47 +0300

    template

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,33 @@
+   
+<!-- header section starts  -->
+<link rel="stylesheet" type="text/css" href="CSS/style.css">
+
+<header>
+
+    <div id="menu-bar" class="fas fa-bars"></div>
+
+    <a href="#" class="logo"><span>T</span>ravel</a>
+
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#book">book</a>
+        <a href="#packages">packages</a>
+        <a href="#services">services</a>
+        <a href="#gallery">gallery</a>
+        <a href="#review">review</a>
+        <a href="#contact">contact</a>
+    </nav>
+
+    <div class="icons">
+        <i class="fas fa-search" id="search-btn"></i>
+        <a href="/log"><i class="fas fa-user"></i></a>
+    </div>
+
+    <form action="" class="search-bar-container">
+        <input type="search" id="search-bar" placeholder="search here...">
+        <label for="search-bar" class="fas fa-search"></label>
+    </form>
+
+</header>
+
+<!-- header section ends -->
